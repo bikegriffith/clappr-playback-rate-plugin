de5ca7903d6505cb33bf32faca58ca21b3cbd584
Mike Griffith <bikegriffith@gmail.com>
2016-01-26 08:57:17 -0500

    works now

--- src/main.js
+++ src/main.js
@@ -2,21 +2,18 @@ import {Events, Styler, UICorePlugin, template} from 'clappr'
 import pluginHtml from './public/playback-rate-selector.html'
 import pluginStyle from './public/style.scss'
 
-const AUTO = -1
+const DEFAULT_PLAYBACK_RATES = [
+  {value: '0.5', label: '0.5x'},
+  {value: '1.0', label: '1x'},
+  {value: '2.0', label: '2x'}
+];
 
 export default class PlaybackRatePlugin extends UICorePlugin {
 
   static get version() { return VERSION }
 
-  get name() { return 'playback_rate' }
-  get template() { 
-    console.log('html', pluginHtml);
-    try {
-      return template(pluginHtml);
-    } catch(e) {
-      console.error(e);
-    }
-  }
+  get name() { return 'playback_rate'; }
+  get template() { return template(pluginHtml); }
 
   get attributes() {
     return {
@@ -49,86 +46,54 @@ export default class PlaybackRatePlugin extends UICorePlugin {
 
   shouldRender() {
     if (!this.core.getCurrentContainer()) {
-      console.warn('playback rate plugin, no container');
       return false;
     }
 
-    var currentPlayback = this.core.getCurrentPlayback()
-    if (!currentPlayback) {
-      console.warn('playback rate plugin, no playback');
-      return false
+    var currentPlayback = this.core.getCurrentPlayback();
+    window.playback = currentPlayback;
+    if (currentPlayback.tagName != 'video' && currentPlayback.tagName != 'audio') {
+      //console.warn('PlaybackRatePlugin#shouldRender: Cannot affect rate for playback', currentPlayback);
+      return false;
     }
 
-    console.warn('playback rate plugin', currentPlayback);
-    // TODO: is HTML5?
-    return true
+    return true;
   }
 
   render() {
-
-    this.playbackRates = [
-      {value: '0.5', label: '0.5x'},
-      {value: '1.0', label: '1x'},
-      {value: '2.0', label: '2x'}
-    ];
+    //console.log('PlaybackRatePlugin#render()');
+    this.playbackRates = DEFAULT_PLAYBACK_RATES;
+    if (!this.selectedRate) {
+      this.selectedRate = '1.0';
+    }
 
     if (this.shouldRender()) {
-      console.log('playback rate plugin rendering');
-      var style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl})
-      console.log('style', style);
+      var t = template(pluginHtml);
+      var html = t({playbackRates: this.playbackRates, title: this.getTitle()});
+      this.$el.html(html);
 
-      try {
-        console.log('trying html', pluginHtml);
-        let t = template(pluginHtml);
-        console.log('created template object', t);
-        let html = t({'playbackRates':this.playbackRates, 'title': this.getTitle()});
-        console.log('rendered', html);
-        this.$el.html(html);
-        console.log('appended html');
-      } catch(e) {
-        console.error(e);
-      }
+      var style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl})
       this.$el.append(style)
-      console.log('appended style');
-      console.log('this.$el', this.$el);
-      console.log('this.el', this.el);
+
       this.core.mediaControl.$('.media-control-right-panel').append(this.el)
-      console.log('appended element');
-      //this.updateText(this.selectedLevelId)
+      this.updateText()
     }
+
     return this
   }
 
-  findLevelBy(id) {
-    var foundLevel
-    this.levels.forEach((level) => { if (level.id === id) {foundLevel = level} })
-    return foundLevel
-  }
-
-  changeLevelLabelBy(id, newLabel) {
-    this.levels.forEach((level, index) => {
-      if (level.id === id) {
-        this.levels[index].label = newLabel
-      }
-    })
-  }
-
   onRateSelect(event) {
+    //console.log('onRateSelect', event.target);
     let rate = event.target.dataset.playbackRateSelect;
-    console.log('selected rate', rate);
 
+    // Set <video playbackRate="..."
     this.core.$el.find('video').get(0).playbackRate = rate;
 
-    /*
-    this.selectedLevelId = parseInt(event.target.dataset.levelSelectorSelect, 10)
-    this.core.getCurrentPlayback().currentLevel = this.selectedLevelId
+    this.selectedRate = rate;
+    this.updateText();
+    this.toggleContextMenu();
 
-    this.updateText(this.selectedLevelId)
-    */
-    this.toggleContextMenu()
-
-    event.stopPropagation()
-    return false
+    event.stopPropagation();
+    return false;
   }
 
   onShowMenu(event) {
@@ -139,35 +104,27 @@ export default class PlaybackRatePlugin extends UICorePlugin {
     this.$('.playback_rate ul').toggle()
   }
 
-  setActiveListItem(level) {
-    console.log(this.$('a'));
+  setActiveListItem(rateValue) {
     this.$(`a`).removeClass('active');
-    this.$(`a[data-playback-rate-select="${level}"`).addClass('active');
-    console.log(this.$('a'));
+    this.$(`a[data-playback-rate-select="${rateValue}"`).addClass('active');
   }
 
   buttonElement() {
-    return this.$('.playback_rate button')
+    return this.$('.playback_rate button');
   }
 
   getTitle() {
-    return '1x';
-    //return (this.core.options.levelSelectorConfig || {}).title
+    let title = this.selectedRate;
+    this.playbackRates.forEach((r) => {
+      if (r.value == this.selectedRate) {
+        title = r.label;
+      }
+    });
+    return title;
   }
 
-  updateText(level) {
-    if (level === AUTO) {
-      var playbackLevel = this.core.getCurrentPlayback().currentLevel;
-      var label = (this.findLevelBy(playbackLevel) || {}).label
-      if (label) {
-        this.buttonElement().text('AUTO (' + label + ')')
-      } else {
-        this.buttonElement().text('AUTO');
-      }
-    } else {
-      var label = (this.findLevelBy(level) || {}).label
-      this.buttonElement().text(label)
-    }
-    this.setActiveListItem(level);
+  updateText() {
+    this.buttonElement().text(this.getTitle())
+    this.setActiveListItem(this.selectedRate);
   }
 }
