4ec16dcfb6520ce135c0fade4f6b1b02eceea35a
Karim Slimani <karim@playmedia.fr>
2020-03-04 16:05:20 +0100

    fix: ensure current and backward compatibility
    Handle rate value as Number.
    Add player external interface methods.
    Add optional rate suffix option.
    Add minor css fix.

--- src/main.js
+++ src/main.js
@@ -2,22 +2,20 @@ import {Events, Styler, UICorePlugin, template} from 'clappr'
 import pluginHtml from './public/playback-rate-selector.html'
 import pluginStyle from './public/style.scss'
 
-
 const DEFAULT_PLAYBACK_RATES = [
-  {value: '0.5', label: '0.5x'},
-  {value: '0.75', label: '0.75x'},
-  {value: '1.0', label: 'Normal'},
-  {value: '1.5', label: '1.5x'},
-  {value: '2.0', label: '2x'}
-];
+  {value: 0.5, label: '0.5x'},
+  {value: 0.75, label: '0.75x'},
+  {value: 1, label: 'Normal'},
+  {value: 1.5, label: '1.5x'},
+  {value: 2, label: '2x'}
+]
 
-const DEFAULT_PLAYBACK_RATE = '1.0';
-
+const DEFAULT_PLAYBACK_RATE = 1
+const DEFAULT_PLAYBACK_RATE_SUFFIX = 'x' // Used by getTitle method
 
 export default class PlaybackRatePlugin extends UICorePlugin {
-
-  get name() { return 'playback_rate'; }
-  get template() { return template(pluginHtml); }
+  get name() { return 'playback_rate' }
+  get template() { return template(pluginHtml) }
 
   get attributes() {
     return {
@@ -33,121 +31,132 @@ export default class PlaybackRatePlugin extends UICorePlugin {
     }
   }
 
-  bindEvents() {
-    this.listenTo(this.core.mediaControl, Events.CORE_ACTIVE_CONTAINER_CHANGED, this.reload);
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render);
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_HIDE, this.hideContextMenu);
-    this.listenTo(this.core.mediaControl, PlaybackRatePlugin.MEDIACONTROL_PLAYBACKRATE, this.updatePlaybackRate);
+  get container() {
+    return this.core.activeContainer
+      ? this.core.activeContainer
+      : this.core.mediaControl.container
   }
 
-  unBindEvents() {
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED);
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_RENDERED);
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_HIDE);
+  get playback() {
+    return this.core.activePlayback
+      ? this.core.activePlayback
+      : this.core.getCurrentPlayback()
+  }
+
+  bindEvents() {
+    if (Events.CORE_ACTIVE_CONTAINER_CHANGED)
+      this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, this.reload)
+    else
+      this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED, this.reload)
+
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_HIDE, this.hideContextMenu)
+  }
+
+  getExternalInterface() {
+    return {
+      getPlaybackRate: this.getSelectedRate,
+      setPlaybackRate: this.setSelectedRate
+    }
   }
 
   reload() {
-    this.unBindEvents();
-    this.bindEvents();
+    this.stopListening()
+    // Ensure it stop listening before rebind events (avoid duplicate events)
+    process.nextTick(() => {
+      this.bindEvents()
+    })
   }
 
   shouldRender() {
-    if (!this.core.activeContainer) {
-      return false;
-    }
-
-    var currentPlayback = this.core.activePlayback;
-    if (currentPlayback.tagName != 'video' && currentPlayback.tagName != 'audio') {
-      //console.warn('PlaybackRatePlugin#shouldRender: Cannot affect rate for playback', currentPlayback);
-      return false;
-    }
-
-    return true;
+    return this.container && (
+      this.playback.tagName === 'video'
+      || this.playback.tagName === 'audio'
+    )
   }
 
   render() {
-    //console.log('PlaybackRatePlugin#render()');
-    const cfg = this.core.options.playbackRateConfig || {};
+    if (! this.shouldRender())
+      return this
 
-    if (!this.playbackRates) {
-      this.playbackRates = cfg.options || DEFAULT_PLAYBACK_RATES;
-    }
+    const cfg = this.core.options.playbackRateConfig || {}
+    this.playbackRates = cfg.options || DEFAULT_PLAYBACK_RATES
+    this.selectedRate = cfg.defaultValue || DEFAULT_PLAYBACK_RATE
+    this.rateSuffix = cfg.rateSuffix || DEFAULT_PLAYBACK_RATE_SUFFIX
 
-    if (!this.selectedRate) {
-      this.selectedRate = cfg.defaultValue || DEFAULT_PLAYBACK_RATE;
-    }
+    let t = template(pluginHtml)
+    let html = t({playbackRates: this.playbackRates, title: this.getTitle()})
+    this.$el.html(html)
 
-    if (this.shouldRender()) {
-      var t = template(pluginHtml);
-      var html = t({playbackRates: this.playbackRates, title: this.getTitle()});
-      this.$el.html(html);
+    let style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl})
+    this.$el.append(style)
 
-      var style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl});
-      this.$el.append(style);
+    this.core.mediaControl.$('.media-control-right-panel').append(this.el)
+    this.updateText()
 
-      this.core.mediaControl.$('.media-control-right-panel').append(this.el);
-      this.updateText();
-    }
-
-    return this;
+    return this
   }
 
   onRateSelect(event) {
-    //console.log('onRateSelect', event.target);
-    let rate = event.target.dataset.playbackRateSelect;
-    this.setSelectedRate(rate);
-    this.toggleContextMenu();
-    event.stopPropagation();
-    return false;
+    event.stopPropagation()
+    let rate = event.target.dataset.playbackRateSelect // data-playback-rate-select
+    this.setSelectedRate(rate)
+    this.toggleContextMenu()
+    return false
   }
 
-  onShowMenu(event) {
-    this.toggleContextMenu();
+  onShowMenu() {
+    this.toggleContextMenu()
   }
 
   toggleContextMenu() {
-    this.$('.playback_rate ul').toggle();
+    this.$('.playback_rate ul').toggle()
   }
 
   hideContextMenu() {
-    this.$('.playback_rate ul').hide();
+    this.$('.playback_rate ul').hide()
   }
 
-  updatePlaybackRate(rate) {
-    this.setSelectedRate(rate);
+  toNumber(value) {
+    value = Number(value)
+    // Fallback to default playback rate if cannot be converted
+    return isNaN(value) ? DEFAULT_PLAYBACK_RATE : value
   }
 
   setSelectedRate(rate) {
-    // Set <video playbackRate="..."
-    this.core.$el.find('video,audio').get(0).playbackRate = rate;
-    this.selectedRate = rate;
-    this.updateText();
+    rate = this.toNumber(rate)
+    this.playback.el.playbackRate = rate
+    this.selectedRate = rate
+    this.updateText()
+  }
+
+  getSelectedRate() {
+    return this.selectedRate
   }
 
   setActiveListItem(rateValue) {
-    this.$(`a`).removeClass('active');
-    this.$(`a[data-playback-rate-select="${rateValue}"]`).addClass('active');
+    this.$('a').removeClass('active')
+    this.$(`a[data-playback-rate-select="${rateValue}"]`).addClass('active')
   }
 
   buttonElement() {
-    return this.$('.playback_rate button');
+    return this.$('.playback_rate button')
   }
 
   getTitle() {
-    let title = this.selectedRate;
-    this.playbackRates.forEach((r) => {
-      if (r.value == this.selectedRate) {
-        title = r.label;
-      }
-    });
-    return title;
+    let rate = this.selectedRate
+
+    for (const i in this.playbackRates) {
+      if (this.playbackRates[i].value == rate)
+        return this.playbackRates[i].label
+    }
+
+    // Unknown rate formatted title
+    return rate + this.rateSuffix
   }
 
   updateText() {
-    this.buttonElement().text(this.getTitle());
-    this.setActiveListItem(this.selectedRate);
+    this.buttonElement().text(this.getTitle())
+    this.setActiveListItem(this.selectedRate)
   }
 }
-
-PlaybackRatePlugin.type = 'core';
-PlaybackRatePlugin.MEDIACONTROL_PLAYBACKRATE = 'playbackRate';
